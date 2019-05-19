d7d2f9b4dbbd11a0dd6d4ac235a2e2f08abafe0a
oliviadian <oliviadiaan12@gmail.com>
2019-05-19 16:57:23 +0700

    Updates

---
+++ precache-manifest.57c4d395125e566d161231e3867c56ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2601468e437375e7a0096f9694926488",
+    "url": "/finalprojectweb3/index.html"
+  },
+  {
+    "revision": "8de50246161d97356d69",
+    "url": "/finalprojectweb3/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "55f5735d0990e7ef00be",
+    "url": "/finalprojectweb3/static/css/main.5a9ff18f.chunk.css"
+  },
+  {
+    "revision": "8de50246161d97356d69",
+    "url": "/finalprojectweb3/static/js/2.271474b4.chunk.js"
+  },
+  {
+    "revision": "55f5735d0990e7ef00be",
+    "url": "/finalprojectweb3/static/js/main.f2e6cd5a.chunk.js"
+  },
+  {
+    "revision": "0ee103d2521efd060fd6",
+    "url": "/finalprojectweb3/static/js/runtime~main.9d9c5e7e.js"
+  }
+]);
